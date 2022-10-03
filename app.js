d161a4203382aa572d1de6192cd2bcd70609413b
huge-pancake <huge.pancake.zxr@gmail.com>
2022-10-03 14:09:19 +0800

    feat: add store

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const routes = {
   '/wiki/accelerator': {},
   '/images': {},
   '/images/misaka-mikoto': {},
+  '/store': {},
+  '/store/mbc': {},
+  '/store/mmsql': {},
+  '/store/mvps': {},
+  '/store/ssfs': {},
 };
 
 /**
